c581d5ddb6de2ce923aecfbb791cda1e1bad77f2
amine sakhri <sakhriaminee2003@gmail.com>
2026-07-20 15:23:42 +0100

    creation les megration et les modles ..

---
+++ app/Http/Controllers/EvenementController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\evenement;
+use App\Http\Requests\StoreevenementRequest;
+use App\Http\Requests\UpdateevenementRequest;
+
+class EvenementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreevenementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(evenement $evenement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(evenement $evenement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateevenementRequest $request, evenement $evenement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(evenement $evenement)
+    {
+        //
+    }
+}
---
+++ app/Models/evenement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class evenement extends Model
+{
+    /** @use HasFactory<\Database\Factories\EvenementFactory> */
+    use HasFactory;
+}
