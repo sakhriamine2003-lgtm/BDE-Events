666261fd9be22160f1c38216576392d2b29a4937
amine sakhri <sakhriaminee2003@gmail.com>
2026-08-13 15:11:47 +0100

    afficher les evenements en utiluse api (axios)

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Evenement from './pages/Evenement'
-import CreationEvenement from './pages/CreeEvenement'
+import AfficherEvenement from './pages/Evenement'
+// import CreationEvenement from './pages/CreeEvenement'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Evenement/>
+    <AfficherEvenement/>
     // <CreationEvenement/>
   )
 }
