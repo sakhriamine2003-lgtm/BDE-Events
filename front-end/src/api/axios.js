b137361718da41af75fb0b374d939260fc70335f
amine sakhri <sakhriaminee2003@gmail.com>
2026-08-13 11:42:50 +0100

    installer react

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:8000/api",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-})
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+// })
 
-export default api;
+// export default api;
